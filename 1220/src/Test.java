4c7841ad8a06c8d12a4fe3d78a5738c6a3e59b31
l-yd <lyd15944591059@outlook.com>
2019-12-20 19:18:25 +0800

    123

--- 1220/src/Test.java
+++ 1220/src/Test.java
@@ -4,12 +4,9 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     System.out.println("newtestlei");
-<<<<<<< HEAD
     System.out.println("我的地盘我做主 ");
     System.out.println("我是刘怡东");
-=======
     System.out.println("lvhufenzhi");
->>>>>>> refs/remotes/origin/lvhu
 	}
 
 }
